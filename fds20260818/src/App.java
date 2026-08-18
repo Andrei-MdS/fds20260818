b4e4105abe5be6c7373971195c011e5b310e9aa7
Marcelo H. Yamaguti <yamaguti@pucrs.br>
2026-08-18 13:40:24 +0000

    Primeiro commit

---
+++ fds20260818/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("FUNDAMENTOS DE DESENVOLVIMENTO DE SOFTWARE");
+        System.out.println("Presenca da aula 18/8/2026: ");
+        // Insira uma linha abaixo que mostre o seu nome na tela
+
+    }
+}
