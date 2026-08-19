d25b9ed13c41c3dbed12dec3fa8cddd7f92f07ad
Igor Xarless <95253660+Xarrles@users.noreply.github.com>
2026-08-19 01:05:17 +0000

    Igor Xavier Nunes

--- fds20260818/src/App.java
+++ fds20260818/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("FUNDAMENTOS DE DESENVOLVIMENTO DE SOFTWARE");
         System.out.println("Presenca da aula 18/8/2026: ");
+        System.out.println("Igor Xavier Nunes - O mais lindo ");
         // Insira uma linha abaixo que mostre o seu nome na tela
 
     }
