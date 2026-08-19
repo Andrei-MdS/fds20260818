33931c0f63ea4da743496e7c2cab1f42e6b7ce04
Pedro Turik Firmino <pedroturik@gmail.com>
2026-08-18 22:13:25 -0300

    presença

--- fds20260818/src/App.java
+++ fds20260818/src/App.java
@@ -5,6 +5,6 @@ public class App {
         System.out.println("Igor Xavier Nunes - O mais lindo ");
         // Insira uma linha abaixo que mostre o seu nome na tela
         System.out.println("Henrique Nunes");
-
+	System.out.println("Pedro Turik Firmino");
     }
 }
