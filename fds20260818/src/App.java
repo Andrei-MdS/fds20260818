5bbf775feffe8d5bd6defacc6ce2d0257f5dda77
Henrique Nunes <henriquerosanunes@hotmail.com>
2026-08-18 22:02:48 -0300

    henrique nunes

--- fds20260818/src/App.java
+++ fds20260818/src/App.java
@@ -3,6 +3,7 @@ public class App {
         System.out.println("FUNDAMENTOS DE DESENVOLVIMENTO DE SOFTWARE");
         System.out.println("Presenca da aula 18/8/2026: ");
         // Insira uma linha abaixo que mostre o seu nome na tela
+        System.out.println("Henrique Nunes");
 
     }
 }
