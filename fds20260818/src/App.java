6a62d99f71518034691493a4ba36adac4ebc0a5e
Tiago-BG <tiago.gregianin@edu.pucrs.br>
2026-08-19 01:08:09 +0000

    Alterações Tiago

--- fds20260818/src/App.java
+++ fds20260818/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("FUNDAMENTOS DE DESENVOLVIMENTO DE SOFTWARE");
         System.out.println("Presenca da aula 18/8/2026: ");
+        System.out.println("Tiago Barbosa Gregianin");
         // Insira uma linha abaixo que mostre o seu nome na tela
 
     }
