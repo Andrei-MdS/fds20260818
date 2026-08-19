d7a2f7b17b67a46bdbb389fe5e93298275fed580
Vitor Lucas dos Santos Reis <vitor.reis03@edu.pucrs.br>
2026-08-19 01:16:38 +0000

    Vitor Presença

--- fds20260818/src/App.java
+++ fds20260818/src/App.java
@@ -6,6 +6,7 @@ public class App {
         System.out.println("Igor Xavier Nunes - O mais lindo ");
         // Insira uma linha abaixo que mostre o seu nome na tela
         System.out.println("Henrique Nunes");
+        System.out.println("Vitor Lucas dos Santos Reis");
 
     }
 }
